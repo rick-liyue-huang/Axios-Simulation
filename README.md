0a11d06748a288c5aeed01033bd6aa40738c91a9
rick-liyue-huang <rick.liyue.huang@gmail.com>
2022-03-25 11:58:10 +1100

    prepare deployment

---
+++ README.md
@@ -0,0 +1,45 @@
+## Using TypeScript Simulate Axios Framework
+
+## Features
+1. Using XMLHttpRequest on browser end;
+2. Support Promise API;
+3. Support request and response interrupter;
+4. Support request data and response data format transformation;
+5. Support request cancellation;
+6. Support JSON data transformation automatically;
+7. Support client end XSS attacking.
+
+
+## Simulation Axios framework Structure
+```markdown
+-- src
+|   |-- axios.ts
+|   |-- defaults.ts
+|   |-- index.ts
+|   |-- cancel
+|   |   |-- Cancel.ts
+|   |   |-- CancelToken.ts
+|   |-- core
+|   |   |-- Axios.ts
+|   |   |-- InterceptorManager.ts
+|   |   |-- dispatchRequest.ts
+|   |   |-- mergeConfig.ts
+|   |   |-- transform.ts
+|   |   |-- xhr.ts
+|   |-- helpers
+|   |   |-- cookie.ts
+|   |   |-- data.ts
+|   |   |-- error.ts
+|   |   |-- headers.ts
+|   |   |-- url.ts
+|   |   |-- util.ts
+|   |-- types
+|       |-- index.ts
+
+```
+
+
+### Example Usage
+
+After running the project, it will show the multiple links, each of which to test the axios simulation is working by checking the console, and each link match the subdirectory name.
+
---
+++ examples/webpack.config.js
@@ -0,0 +1,74 @@
+const fs = require('fs')
+const path = require('path')
+const webpack = require('webpack')
+
+module.exports = {
+  mode: 'development',
+
+  /**
+   * create multiple subdirectories under examples
+   * show different demo in subdirectories
+   * create app.ts under each subdirectory
+   * app.ts is the entry file of  webpack bundle
+   * entries collect multiple subdirectory entry files
+   * entries is object，key is directory name
+   */
+  entry: fs.readdirSync(__dirname).reduce((entries, dir) => {
+    const fullDir = path.join(__dirname, dir)
+    const entry = path.join(fullDir, 'app.ts')
+    if (fs.statSync(fullDir).isDirectory() && fs.existsSync(entry)) {
+      entries[dir] = ['webpack-hot-middleware/client', entry]
+    }
+
+    return entries
+  }, {}),
+
+  /**
+   * create different directory name and bundle to js file with the same name
+   */
+  output: {
+    path: path.join(__dirname, '__build__'),
+    filename: '[name].js',
+    publicPath: '/__build__/'
+  },
+
+  module: {
+    rules: [
+      {
+        test: /\.ts$/,
+        enforce: 'pre',
+        use: [
+          {
+            loader: 'tslint-loader'
+          }
+        ]
+      },
+      {
+        test: /\.tsx?$/,
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true
+            }
+          }
+        ]
+      },
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 'css-loader'
+        ]
+      }
+    ]
+  },
+
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js']
+  },
+
+  plugins: [
+    new webpack.HotModuleReplacementPlugin(),
+    new webpack.NoEmitOnErrorsPlugin()
+  ]
+}
